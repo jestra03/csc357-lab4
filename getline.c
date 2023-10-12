3c8b5a8e0b86170c19ab56a0b38d54d77d04eb4f
jestra03 <joshemestra@gmail.com>
2023-10-12 13:05:23 -0700

    remove *alloc usuage (explicit memory allocation

--- getline.c
+++ getline.c
@@ -1,81 +1,47 @@
 #define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 ssize_t remove_newline(char *buffer, ssize_t input);
 
 int main(int argc, char *argv[]) {
-	size_t t_bufsize;
-	char *t_buffer;
-	ssize_t t_chars;
+	FILE *fp;  // file
+	fp = fopen(argv[1], "r");
 	
-	size_t bufsize = 10;
-	char *buffer = (char*)malloc(bufsize*sizeof(char));
-	ssize_t characters = 0;
+	size_t bufsize = 120;
 
-	size_t bufsize1 = 10;
-	char *buffer1 = (char*)malloc(bufsize*sizeof(char));
-	ssize_t characters1 = 0;
+	char buffer0[bufsize];
+	char *b0 = buffer0;
+	ssize_t chars0 = 0;
 
-	FILE *fp;  // file pointer	
-	fp = fopen(argv[1], "r");  // read from arg 1 ("filename.txt")
+	char buffer1[bufsize];
+	char *b1 = buffer1;
+	ssize_t chars1 = 0;	
 
 	if (fp == NULL) {
-		printf("File Operation unsuccessful\n");
-		free(buffer);
-		free(buffer1);
+		printf("File Operation Unsuccessful\n");
 		return -1;
 	}
 
-	// get first line, return if none	
-	characters = getline(&buffer, &bufsize, fp);
-	if (characters == -1) {
-		// printf("File is empty");
-		free(buffer);
-		free(buffer1);
-		fclose(fp);
-		return 1;
+        chars0 = getline(&b0, &bufsize, fp);
+        if (chars0 == -1) {
+                printf("could not read line");
+                return -1;
+        }
+
+	chars1 = getline(&b1, &bufsize, fp);
+	if (chars1 == -1) {
+		remove_newline(b0, chars1);
+		printf("%s", b0);
+		return -1;
 	}
 
-	// get second line, print first line if no second
-	remove_newline(buffer, characters);
-        characters1 = getline(&buffer1, &bufsize1, fp);
-	if (characters1 == -1) {
-		printf("%s\n", buffer);
-		free(buffer);
-		free(buffer1);
-		fclose(fp);
-		return 1;
-	}
-	remove_newline(buffer1, characters1);
+
+	// while loop
 	
-	
-	// while True
-	// get next line
-	// if line is EOF print 1st 2nd, return
-	// else replace 1st with 2nd, set 2nd to current line
+	fclose(fp);
 
-	// initialize temp heap 
-	t_bufsize = 10;
-        t_buffer = (char*)malloc(bufsize*sizeof(char));
-        t_chars = 0;
-
-	// read loop here
-
-
-	printf("%s\n", buffer); // print first line
-	printf("%s\n", buffer1); // print second line
-	// while True
-	free(buffer);
-	free(buffer1);
-	free(t_buffer);
-	fclose(fp);  // close file
-	
-	
-
-
-	return 0;
 }
-
 ssize_t remove_newline(char *buffer, ssize_t input) {
 	if (buffer[input - 1] == '\n') {
 		buffer[input - 1] = '\0';
