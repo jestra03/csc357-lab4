b52ee3039c792464b491994ba3fcf21cfc0fc9a2
jestra03 <joshemestra@gmail.com>
2023-10-13 10:12:10 -0700

    read.c finished

--- read.c
+++ read.c
@@ -4,12 +4,12 @@
 #include <unistd.h>
 #include <fcntl.h>
 
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[]) {
 	int fd;  // file descriptor
 	char *endptr;	
 	int BUFFER_SIZE = strtol(argv[2], &endptr, 10);
-	if (endptr == NULL) {
-		printf("Invalid arg");
+	if (*endptr != '\0') {
+		printf("Invalid Arg\n");
 		return -1;
 	}	
 	char buffer[BUFFER_SIZE];
@@ -17,17 +17,16 @@ int main(int argc, char *argv[]) {
 
 	fd = open(argv[1], O_RDONLY);
 	if (fd == -1) {
-		printf("File Operation unsuccessful");
+		printf("File Operation unsuccessful\n");
 		return -1;
 	}
 
 	characters = read(fd, buffer, BUFFER_SIZE);
 	if (characters == -1) {
-		printf("Error file not read");
+		printf("Error file not read\n");
 		close(fd);
 		return -1;
-	}	
-	
+	}		
 	close(fd);
 	return 0;
 }
